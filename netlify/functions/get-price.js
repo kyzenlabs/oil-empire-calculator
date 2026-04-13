e7f38b3c212a365fd996a9a50f40bacf7d05c062
kyzen <lusense2024@gmail.com>
2026-04-13 01:15:23 -0400

    Create get-price.js

---
+++ netlify/functions/get-price.js
@@ -0,0 +1,43 @@
+let { getState } = (() => {
+  try {
+    return require("./send-price");
+  } catch {
+    return { getState: () => ({ latestPrice: null, lastUpdated: null }) };
+  }
+})();
+
+exports.handler = async () => {
+  const { latestPrice, lastUpdated } = getState();
+
+  if (latestPrice === null) {
+    return {
+      statusCode: 404,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({
+        ok: false,
+        error: "No price received yet",
+        price: null,
+        updatedAt: null,
+      }),
+    };
+  }
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+
+      "Access-Control-Allow-Origin": "*",
+   
+      "Cache-Control": "no-store",
+    },
+    body: JSON.stringify({
+      ok: true,
+      price: latestPrice,
+      updatedAt: lastUpdated,
+    }),
+  };
+};
